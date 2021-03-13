110b091ed5c8e72679f6ac41dafb09fb8fa3b521
Emre Demir <emre2548@gmail.com>
2021-03-13 15:19:28 +0300

    10 Mart commit Part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'northwind';
   user: string = "Emre DEMİR"
+  product1 = {product:1,productName:'Bardak',categoryId:1, unitPrice:5}
+  product2 = {product:2,productName:'Laptop',categoryId:1, unitPrice:5}
+  product3 = {product:3,productName:'Mouse',categoryId:1, unitPrice:5}
+  product4 = {product:4,productName:'Keyboard',categoryId:1, unitPrice:5}
+  product5 = {product:5,productName:'Camera',categoryId:1, unitPrice:5}
 
+  products = [this.product1,this.product2,this.product3,this.product4,this.product5]
 }
