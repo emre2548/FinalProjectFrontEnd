79487806ec6a8e9966dbc2fdd41b5e3d5123dc06
Emre Demir <emre2548@gmail.com>
2021-03-21 11:40:46 +0300

    20 Mart Commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full",component:ProductComponent}, // Ana sayfada rooteroutlet de ne göstereyim
   {path:"products",component:ProductComponent},
-  {path:"products/category/:categoryId",component:ProductComponent}
+  {path:"products/category/:categoryId",component:ProductComponent},
+  {path:"products/add", component:ProductAddComponent}
 ];
 
 @NgModule({
